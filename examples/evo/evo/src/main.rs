82a769931ab0a69489310da63c4d2aee3667d535
David McDougall <dam1784@rit.edu>
2026-09-01 17:32:12 -0400

    Evolution program - WIP

--- examples/evo/evo/src/main.rs
+++ examples/evo/evo/src/main.rs
@@ -18,10 +18,10 @@ use std::ffi::OsStr;
 use std::fs;
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::path::{Path, PathBuf};
-use std::sync::{Arc, Mutex};
 
 fn main() {
-    let mut evo = Evolution::new(std::env::args().collect()).unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    let mut evo = Evolution::new(args).unwrap();
     evo.main().unwrap();
 }
 
@@ -40,7 +40,9 @@ pub struct Evolution {
 
     score: String,
 
-    selection: String,
+    selection: Vec<String>,
+
+    selection_fn: SelectionFn,
 
     replacement: Replacement,
 
@@ -54,29 +56,18 @@ pub struct Evolution {
 
     generation: u64,
 
-    members: Vec<Individual>,
+    population: Vec<Individual>,
 
     waiting: Vec<Individual>,
 
-    leaderboard: Vec<(f64, u64, PathBuf)>,
+    leaderboard: Vec<Individual>,
 
     parents: Vec<Vec<PathBuf>>,
 
     verbose: bool,
 }
 
-/// Persistent storage for parameters and program state
-#[derive(Serialize, Deserialize)]
-struct Metadata {
-    replacement: Replacement,
-    selection: String,
-    score: String,
-    population_size: usize,
-    leaderboard_size: usize,
-    hall_of_fame_size: usize,
-    ascension: u64,
-    generation: u64,
-}
+type SelectionFn = Box<dyn MateSelection<rand::rngs::ThreadRng>>;
 
 /// Controls how the population replaces individuals
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq)]
@@ -85,20 +76,32 @@ pub enum Replacement {
     Frozen,
 
     /// Do not replace members, the population grows without bounds
-    Unbounded,
+    Growth,
 
     /// Replace members at random
     Random,
 
-    /// Replace the oldest member
+    /// Replace the oldest members
     Oldest,
 
-    /// Replace the lowest scoring member
+    /// Replace the lowest scoring members
     Worst,
 
-    // TODO: Rename generation to cohort?
-    /// Replace generations entirely and all at once
+    /// Replace each generation entirely and all at once
     Generation,
+}
+
+/// Persistent storage for parameters and program state
+#[derive(Serialize, Deserialize)]
+struct Metadata {
+    score: String,
+    selection: Vec<String>,
+    replacement: Replacement,
+    population_size: usize,
+    leaderboard_size: usize,
+    hall_of_fame_size: usize,
+    ascension: u64,
+    generation: u64,
 }
 
 /// Getter / setter methods
@@ -131,7 +134,7 @@ impl Evolution {
         self.score.clone()
     }
     /// Get the `selection` argument
-    pub fn get_selection(&self) -> String {
+    pub fn get_selection(&self) -> Vec<String> {
         self.selection.clone()
     }
     /// Get the `replacement` argument
@@ -166,23 +169,10 @@ impl Evolution {
     ///
     /// This accepts the program's CLI arguments
     pub fn new(mut args: Vec<String>) -> Result<Self, Error> {
-        // Discard the name of the program
-        if !args.is_empty() {
-            let _prog = args.remove(0);
-        }
-        // Split the file path from args, if one was given
-        let arg0 = args.get(0);
-        let mut path = None;
-        if let Some(arg0) = arg0 {
-            if !arg0.starts_with('-') {
-                path = Some(PathBuf::from(arg0));
-                args.remove(0);
-            }
-        }
         // Initialize or load from file
         let mut this = Self::default();
-        if let Some(path) = path
-            && !path.is_empty()
+        if let Some(path) = Self::parse_args(&mut args)
+            && !path.as_os_str().is_empty()
         {
             if !path.exists() {
                 this.init(path)?;
@@ -194,7 +184,7 @@ impl Evolution {
             this.init(path)?;
         }
         // Apply the remaining CLI arguments
-        if this.parse_flags(args) {
+        if this.parse_flags(&mut args) {
             // Update the save file with the new parameters
             this.save()?;
         }
@@ -203,15 +193,16 @@ impl Evolution {
     fn default() -> Self {
         Self {
             path: PathBuf::new(),
-            replacement: todo!(),
-            selection: todo!(),
+            replacement: Replacement::Generation,
+            selection: vec!["exponential".to_string(), "10".to_string()],
+            selection_fn: Box::new(mate_selection::RankedExponential(10)),
             score: "score".to_string(),
             population_size: 100,
             leaderboard_size: 10,
             hall_of_fame_size: 0,
             ascension: 0,
             generation: 0,
-            members: vec![],
+            population: vec![],
             waiting: vec![],
             leaderboard: vec![],
             parents: vec![],
@@ -224,8 +215,24 @@ impl Evolution {
         path.push(format!("evo{:x}", rand::random_range(0..u64::MAX)));
         path
     }
+    /// Process the first 2 arguments (program-name and save-dir)
+    fn parse_args(args: &mut Vec<String>) -> Option<PathBuf> {
+        // Discard the name of the program
+        if !args.is_empty() {
+            let _prog = args.remove(0);
+        }
+        // Split the file path from flag arguments, if path was given
+        let mut path = None;
+        if let Some(arg0) = args.get(0) {
+            if !arg0.starts_with("-") {
+                path = Some(PathBuf::from(arg0));
+                args.remove(0);
+            }
+        }
+        path
+    }
     /// Parse the command line arguments
-    fn parse_flags(&mut self, mut args: Vec<String>) -> bool {
+    fn parse_flags(&mut self, args: &mut Vec<String>) -> bool {
         let mut update = false;
         while !args.is_empty() {
             let flag = args.remove(0).to_lowercase();
@@ -240,12 +247,15 @@ impl Evolution {
                     update = true;
                 }
                 "-s" | "--selection" => {
-                    todo!();
-                    update = true;
+                    let (selection, selection_fn) = Self::parse_selection(args);
+                    update = selection != self.selection;
+                    self.selection = selection;
+                    self.selection_fn = selection_fn;
                 }
                 "-r" | "--replacement" => {
-                    todo!();
-                    update = true;
+                    let value = Replacement::parse(args);
+                    update = value != self.replacement;
+                    self.replacement = value;
                 }
                 "-l" | "--leaderboard" => {
                     let value: usize = args.remove(0).parse().unwrap();
@@ -262,38 +272,69 @@ impl Evolution {
                 }
                 "--version" => {
                     println!("{}", VERSION);
+                    std::process::exit(0);
                 }
                 "-h" | "--help" => {
                     println!("{}", HELP);
+                    std::process::exit(0);
                 }
                 _ => {
                     eprintln!("{}", USAGE);
+                    std::process::exit(1);
                 }
             }
         }
         update
     }
+    fn parse_selection(args: &mut Vec<String>) -> (Vec<String>, SelectionFn) {
+        let mut selection = vec![args.remove(0).to_lowercase()];
+        let selection_fn: SelectionFn = match selection[0].as_str() {
+            "best" => {
+                selection.push(args.remove(0));
+                Box::new(mate_selection::Best(selection[1].parse().unwrap()))
+            }
+            "normal" => todo!(),
+            "percent" => todo!(),
+            "score" => todo!(),
+            "random" => todo!(),
+            "ranked" => todo!(),
+            "exponential" => {
+                selection.push(args.remove(0));
+                Box::new(mate_selection::RankedExponential(
+                    selection[1].parse().unwrap(),
+                ))
+            }
+            arg0 => panic!(
+                "unexpected selection type, expected on of ... found {}",
+                arg0
+            ),
+        };
+        (selection, selection_fn)
+    }
     /// Initialize file & directory structures
     fn init(&mut self, path: PathBuf) -> Result<(), Error> {
-        fs::create_dir(&path)?;
+        self.path = path;
+        fs::create_dir(&self.path)?;
         fs::create_dir(self.get_population_path())?;
         fs::create_dir(self.get_waiting_path())?;
         fs::create_dir(self.get_leaderboard_path())?;
         fs::create_dir(self.get_hall_of_fame_path())?;
+        self.save()?;
         Ok(())
     }
+    /// Write metadata file
     fn save(&self) -> Result<(), Error> {
         let metadata = Metadata {
-            replacement: self.get_replacement(),
-            selection: self.get_selection(),
             score: self.get_score(),
+            selection: self.get_selection(),
+            replacement: self.get_replacement(),
             population_size: self.get_population_size(),
             leaderboard_size: self.get_leaderboard_size(),
             hall_of_fame_size: self.get_hall_of_fame_size(),
             ascension: self.get_ascension(),
             generation: self.get_generation(),
         };
-        let metadata = serde_json::to_vec(&metadata)?;
+        let metadata = serde_json::to_vec_pretty(&metadata)?;
         let path = self.get_metadata_path();
         // Write to temporary file and rename for atomic file update
         let mut tmp = path.clone();
@@ -303,60 +344,76 @@ impl Evolution {
         Ok(())
     }
     fn load(&mut self, path: PathBuf) -> Result<(), Error> {
-        let path = self.get_metadata_path();
-        let json = fs::read(&path)?;
-        let mut metadata: Metadata = serde_json::from_slice(&json).unwrap();
+        self.path = path;
+        let json = fs::read(&self.get_metadata_path())?;
+        let mut metadata: Metadata = serde_json::from_slice(&json)?;
         self.replacement = metadata.replacement;
-        self.selection = std::mem::take(&mut metadata.selection);
+        (self.selection, self.selection_fn) =
+            Self::parse_selection(&mut std::mem::take(&mut metadata.selection));
         self.score = std::mem::take(&mut metadata.score);
         self.population_size = metadata.population_size;
         self.leaderboard_size = metadata.leaderboard_size;
         self.hall_of_fame_size = metadata.hall_of_fame_size;
         self.ascension = metadata.ascension;
         self.generation = metadata.generation;
-        fn scan_for_indiv(path: PathBuf) -> Result<Vec<PathBuf>, Error> {
-            let mut files = vec![];
-            for entry in fs::read_dir(path)? {
-                let entry = entry?;
-                let path = entry.path();
-                if path.extension() == Some(OsStr::new("indiv")) {
-                    files.push(path);
-                }
-            }
-            Ok(files)
-        }
-        todo!();
-        // self.members = scan_for_indiv(self.get_population_path())?;
-        // self.waiting = scan_for_indiv(self.get_waiting_path())?;
-        // //
-        // let leaderboard = scan_for_indiv(self.get_leaderboard_path())?;
-        // self.leaderboard.sort_unstable_by(todo!());
+        self.population = Individual::load_dir(self.get_population_path())?;
+        self.waiting = Individual::load_dir(self.get_waiting_path())?;
+        self.leaderboard = Individual::load_dir(self.get_leaderboard_path())?;
+        // self.leaderboard.sort_unstable_by();
+        // todo!();
         Ok(())
+    }
+}
+impl Replacement {
+    fn parse(args: &mut Vec<String>) -> Replacement {
+        match args.remove(0).to_lowercase().as_str() {
+            "generation" => Replacement::Generation,
+            "worst" => Replacement::Worst,
+            "oldest" => Replacement::Oldest,
+            "random" => Replacement::Random,
+            "growth" => Replacement::Growth,
+            "frozen" => Replacement::Frozen,
+            arg0 => {
+                panic!("Expected one of ..., found {}", arg0)
+            }
+        }
     }
 }
 
 impl API for Evolution {
     /// Get a list of parents to be mated together to produce a child.
     fn spawn(&mut self) -> Vec<PathBuf> {
-        // // Refill parents buffer.
-        // if self.parents.is_empty() {
-        //     let num_pairs = if self.get_replacement() == Replacement::Generation {
-        //         self.get_population_size()
-        //     } else {
-        //         1
-        //     };
-        //     let members = self.get_members();
-        //     assert!(!members.is_empty(), "population is empty");
-        //     todo!();
-        //     // self.parents
-        //     //     .extend_from_slice(&(*self.selection)(members, num_pairs));
-        // }
-        // let mut parents = self.parents.pop().unwrap();
-        // // Deduplicate the parents list.
-        // parents.sort_unstable_by_key(Arc::as_ptr);
-        // parents.dedup_by_key(|parent| Arc::as_ptr(parent));
-        // parents
-        todo!()
+        if self.population.is_empty() {
+            return vec![];
+        }
+        // Refill parents buffer.
+        if self.parents.is_empty() {
+            let rng = &mut rand::rng();
+            let buffer_size = match self.replacement {
+                Replacement::Generation | Replacement::Frozen => self.population_size,
+                _ => 1,
+            };
+            let scores: Vec<f64> = self
+                .population
+                .iter()
+                .map(|individual| self.score_fn(individual))
+                .collect();
+            let mut index = self.selection_fn.pairs(rng, buffer_size, scores);
+            // Deduplicate all of the parents pairs.
+            // for pair in index.iter_mut() {
+            //     pair.sort_unstable();
+            //     pair.dedup();
+            // }
+            self.parents.reserve(index.len());
+            for pair in index {
+                self.parents.push(
+                    pair.iter()
+                        .map(|&idx| self.population[idx].path.as_ref().unwrap().clone())
+                        .collect(),
+                );
+            }
+        }
+        self.parents.pop().unwrap()
     }
     /// Add a new individual to this population.
     fn death(&mut self, mut individual: PathBuf) {
@@ -433,7 +490,11 @@ impl API for Evolution {
         }
         Default::default()
     }
-    fn quit(&mut self) {}
+}
+impl Evolution {
+    fn score_fn(&self, individual: &Individual) -> f64 {
+        individual.score.as_ref().unwrap().parse().unwrap()
+    }
 }
 
 /*
@@ -457,23 +518,6 @@ impl API for Evolution {
 /// | 3+ | Unspecified |
 ///
 fn foobar() {}
-
-// fn default_selection(population_size: usize, score: Arc<Score>) -> Arc<Selection> {
-//     const MEDIAN_PERCENT: f64 = 0.1;
-//     let median = (population_size as f64 * MEDIAN_PERCENT).round() as usize;
-//     Arc::new(move |population, spawn| {
-//         let rng = &mut rand::rng();
-//         let scores: Vec<f64> = population
-//             .iter()
-//             .map(|individual| score(&individual.lock().unwrap()))
-//             .collect();
-//         let index = mate_selection::RankedExponential(median).pairs(rng, spawn, scores);
-//         index
-//             .iter()
-//             .map(|parents| parents.iter().map(|&i| population[i].clone()).collect())
-//             .collect()
-//     })
-// }
 
 /// Individuals may have custom score functions with this type signature.
 ///
